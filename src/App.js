59b09820f7b6376bc411cb5ff198a65e96847769
theplew14412 <pew_woothiphom@hotmail.com>
2021-08-10 13:15:02 +0700

    complete todo app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>To Do App</h1>
+      <TodoList />
     </div>
   );
 }
